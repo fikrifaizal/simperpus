fd94b439597021e0ec82db7fe1ba9264f85b4be3
fikrifaizal <fikrifaizal40@gmail.com>
2021-12-14 19:11:57 +0700

    test deploy

--- index.php
+++ index.php
@@ -1,4 +1,47 @@
+<?php 
+//cek login
+// user
+if(isset($_COOKIE['id'])){
+  $id = $_COOKIE['id'];
+  $keterangan = $_COOKIE['keterangan'];
 
+  if($keterangan == "user"){
+    $sql = "SELECT `nomor_anggota`, `nama` FROM anggota
+            WHERE `nomor_anggota`='$id'";
+
+    $result = mysqli_query($conn, $sql);
+    $data = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    if(!empty($data)){
+      //jika valid, set session login
+      $_SESSION['id'] = $data['nomor_anggota'];
+      $_SESSION['nama'] = $data['nama'];
+      $_SESSION['keterangan'] = "user";
+
+      header('location: account\user\home.php');
+      exit();
+    }
+  }
+  // admin
+  elseif($keterangan == "admin"){  
+    $sql = "SELECT `nomor_petugas`, `nama` FROM petugas
+            WHERE `nomor_petugas`='$id'";
+  
+    $result = mysqli_query($conn, $sql);
+    $data = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    
+    if(!empty($data)){
+      //jika valid, set session login
+      $_SESSION['id'] = $data['nomor_petugas'];
+      $_SESSION['nama'] = $data['nama'];
+      $_SESSION['keterangan'] = "admin";
+
+      header('location: account\admin\home.php');
+      exit();
+    }
+  }
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
